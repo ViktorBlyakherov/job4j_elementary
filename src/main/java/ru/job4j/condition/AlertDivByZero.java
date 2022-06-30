1ac21d59742a10fbf612999c936e5f9b130f4635
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-06-30 10:14:17 +0300

    4.1. Операторы ветвлений [#3611 #317345]rev2

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -8,7 +8,6 @@ public class AlertDivByZero {
     }
 
     public static void possibleDiv(int number) {
-        boolean result = number == 0;
         if (number == 0) {
             System.out.println("Could not div by 0.");
         } else if (number < 0) {
