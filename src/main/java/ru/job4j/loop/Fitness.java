244e3f3d47c8379d6f3fe1a43ee622fe78f9cf73
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-07-04 14:01:00 +0300

    5.4. Протеиновая диета [#156313]

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+import com.sun.jdi.VMOutOfMemoryException;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
+            month++;
+        }
+
+        return month;
+    }
+}
